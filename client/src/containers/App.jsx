171c6fedadb79806fa5bae6d9bd769b24da5b2ca
utkarsh032 <utkarshraj525@gmail.com>
2024-02-14 09:45:31 +0530

    ReactQueryDevtools

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -1,51 +1,45 @@
 import { Suspense, useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 
 import { Home, UserProfile, AdminHome, Authentication } from '../pages'
 import { Layout, AdminLayout, AuthLayout } from '../layouts'
-import { auth } from '../config/firebase.config';
-
-
 
 const App = () => {
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((userCred) => {
-      if (userCred) {
-        userCred.getIdToken().then((token) => {
-          console.log(token);
-        });
-      }
-    });
-    return () => {
-      unsubscribe();
-    };
-  }, [auth]);
+
+  const queryClient = new QueryClient()
 
   return (
-    <Router>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          {/*client */}
-          <Route element={<Layout />}>
-            <Route path='/' element={<Home />} />
-            <Route path='/profile/:uid' element={<UserProfile />} />
-          </Route>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            {/*client */}
+            <Route element={<Layout />}>
+              <Route path='/' element={<Home />} />
+              <Route path='/profile/:uid' element={<UserProfile />} />
+            </Route>
 
-          {/*admin */}
-          <Route path='/admin/*' element={<AdminLayout />}>
-            <Route index element={<AdminHome />} />
-          </Route>
+            {/*admin */}
+            <Route path='/admin/*' element={<AdminLayout />}>
+              <Route index element={<AdminHome />} />
+            </Route>
 
-          {/*user*/}
-          <Route path='/auth/*' element={<AuthLayout />}>
-            <Route index element={<Authentication />} />
-          </Route>
+            {/*user*/}
+            <Route path='/auth/*' element={<AuthLayout />}>
+              <Route index element={<Authentication />} />
+            </Route>
 
 
-        </Routes>
-      </Suspense>
-    </Router>
+          </Routes>
+        </Suspense>
+      </Router>
+
+      {/*enable the dev tools to handle the state library*/}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
