d005f88ba517cc334ad7116b6f6c989673e67974
utkarsh032 <utkarshraj525@gmail.com>
2024-02-14 09:33:15 +0530

    collectdata decodedToken.

--- server/functions/index.js
+++ server/functions/index.js
@@ -14,7 +14,7 @@ exports.validateUserJWTToken = functions.https.onRequest(async (req, res) => {
   // enabling the cors
   cors(req, res, async () => {
     // Get the authorization header from the request
-    const authorizationHeader = req.get('Authorization');
+    const authorizationHeader = req.get("Authorization");
 
     // check if the authorization header is present
     if (!authorizationHeader || !authorizationHeader.startsWith("Bearer")) {
@@ -27,13 +27,21 @@ exports.validateUserJWTToken = functions.https.onRequest(async (req, res) => {
     try {
       const decodedToken = await admin.auth().verifyIdToken(token);
       if (decodedToken) {
+        const docRef = db.collection("users").doc(decodedToken.uid)
+        const doc = await docRef.get()
+
+        if (!doc.exists) {
+          const userRef = await db.collection("users").doc(decodedToken.uid)
+          await userRef.set(decodedToken)
+        }
+
         return res.status(200).json({ success: true, user: decodedToken });
       }
     } catch (error) {
       console.log("Error on validating :", error);
       return res
-      .status(402)
-      .json({error: error.message, status: "Unauthorized"});
+        .status(402)
+        .json({ error: error.message, status: "Unauthorized" });
     }
   });
 });
